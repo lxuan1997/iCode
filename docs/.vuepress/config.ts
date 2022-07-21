ece6300e326285d667c7f22a9af759ec03275f36
lxuan <lxuan2497@gmail.com>
2022-07-22 01:07:10 +0800

    base修改

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,7 @@ import { sidebar, navbar, head } from './configs'
 import { defaultTheme } from 'vuepress'
 
 export default defineUserConfig({
+  base: '/note/',
   lang: 'zh-CN',
   title: 'Hello 🖐！',
   description: '',
