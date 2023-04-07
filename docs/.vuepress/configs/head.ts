2e4e7b688189c107577747e8c4297e816b37d317
luoxuan <lxuan2497@gmail.com>
2023-04-07 18:20:35 +0800

    update

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -2,20 +2,21 @@ import type { HeadConfig } from "vuepress";
 const baseUrl = process.env.NODE_ENV === 'development' ? '' : '/notes'
 
 export const head: HeadConfig[] = [
-  [
-    'link', {
-      rel: 'icon',
-      href: `${baseUrl}/logo.png`
+    [
+        'link', {
+        rel: 'icon',
+        href: `${baseUrl}/logo-icon.png`
     }
-  ],
-  [
-    'link',
-    { rel: 'apple-touch-icon', href: `${baseUrl}/logo.png` },
-  ],
-  [
-    'script', {
-      type: 'text/javascript',
-      src: 'https://api.map.baidu.com/api?v=2.0&ak=pvMaEosAuzivPs9B3avkYEjo9DxlCZPs'
+    ],
+    // 添加apple icon
+    [
+        'link',
+        { rel: 'apple-touch-icon', href: `${baseUrl}/logo-icon.png` },
+    ],
+    [
+        'script', {
+        type: 'text/javascript',
+        src: 'https://api.map.baidu.com/api?v=2.0&ak=pvMaEosAuzivPs9B3avkYEjo9DxlCZPs'
     }
-  ]
+    ]
 ]
